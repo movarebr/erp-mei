e5f1bc5583c046263950f130e5c9684a0cec5db2
Movare <contatomoreiraservicos@gmail.com>
2026-05-04 17:55:08 -0300

    Create utils.ts

---
+++ src/lib/utils.ts
@@ -0,0 +1,25 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatCurrency(value: number): string {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value)
+}
+
+export function formatDate(date: string | Date): string {
+  return new Intl.DateTimeFormat('pt-BR').format(new Date(date))
+}
+
+export function getMonthName(month: number): string {
+  const months = [
+    'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+  ]
+  return months[month - 1]
+}
